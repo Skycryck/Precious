cb4cec4b3c3b2e52a4c9f78954ab08c999f6eddf
Jules Vanhulst <vanhulst79@gmail.com>
2023-11-07 12:49:49 +0100

    The One make you invisible when equip

--- src/main/java/net/skycryck/precious/item/custom/TheOneItem.java
+++ src/main/java/net/skycryck/precious/item/custom/TheOneItem.java
@@ -2,15 +2,19 @@ package net.skycryck.precious.item.custom;
 
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
+import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
 import org.jetbrains.annotations.Nullable;
+import top.theillusivec4.curios.api.SlotContext;
+import top.theillusivec4.curios.api.type.capability.ICurioItem;
 
 import java.util.List;
 
-public class TheOneItem  extends Item {
+public class TheOneItem  extends Item implements ICurioItem {
     public TheOneItem(Properties pProperties) {
         super(pProperties);
     }
@@ -24,5 +28,19 @@ public class TheOneItem  extends Item {
         }
 
         super.appendHoverText(pStack, pLevel, pTooltipComponents, pIsAdvanced);
+    }
+
+    @Override
+    public void onEquip(SlotContext slotContext, ItemStack prevStack, ItemStack stack) {
+        LivingEntity player = slotContext.entity();
+        player.setInvisible(true);
+        ICurioItem.super.onEquip(slotContext, prevStack, stack);
+    }
+
+    @Override
+    public void onUnequip(SlotContext slotContext, ItemStack newStack, ItemStack stack) {
+        LivingEntity player = slotContext.entity();
+        player.setInvisible(false);
+        ICurioItem.super.onUnequip(slotContext, newStack, stack);
     }
 }
